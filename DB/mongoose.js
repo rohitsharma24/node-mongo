c70d4a6cd48b71e86e3423e7f34d528e6723e5db
Rohit <rohit.sharma.2495@outlook.com>
2018-06-26 03:08:14 +0530

    add mongodb uri

--- DB/mongoose.js
+++ DB/mongoose.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 mongoose.promise = global.promise;
 
-mongoose.connect('mongodb://localhost:27017/TodoApp', (err, client)=> {
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp';
+mongoose.connect(MONGODB_URI, (err, client)=> {
     if(err) {
         console.log('Error while connecting to MongoDB Server', err);
         return {};
