b7ec82e1d9088ba51fe262495e473af3240334d9
Rohit <rohit.sharma.2495@outlook.com>
2018-06-26 23:52:46 +0530

    add expect for number check

--- test/server.test.js
+++ test/server.test.js
@@ -141,7 +141,7 @@ describe('PATCH /todos/:id', () => {
             .expect((res) => {
                 expect(res.body.todo.text).toBe(text);
                 expect(res.body.todo.completed).toBe(true);
-                //expect(res.body.todo.completedAt).toBeA(Number);
+                expect(typeof res.body.todo.completedAt).toBe('number');
             })
             .end((err, res) => {
                 if(err) {
